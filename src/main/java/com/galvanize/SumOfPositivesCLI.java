db0e5421308e192c40c78daab3f1e0142fbb71fd
Kyle Ortega <kortega@m-c02fq6scmd6m.corp.svbank.com>
2022-08-08 12:57:15 -0700

    logic added

--- src/main/java/com/galvanize/SumOfPositivesCLI.java
+++ src/main/java/com/galvanize/SumOfPositivesCLI.java
@@ -1,4 +1,16 @@
 package com.galvanize;
 
 public class SumOfPositivesCLI {
+    public static void main(String[] args) {
+       int sum = 0;
+
+       for (int i = 0; i < args.length; i++) {
+           int tempNumb = Integer.parseInt(args[i]);
+           if (tempNumb > 0) {
+               sum+=tempNumb;
+           }
+       }
+
+        System.out.println(sum);
+    }
 }
